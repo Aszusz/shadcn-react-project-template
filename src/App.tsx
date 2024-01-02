e45563fab6e75bd60c3dddcb724aa37e5dc2b5b4
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-01-02 19:39:37 +0100

    Make tailwindcss intellisense work with cva

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1 className="text-4xl">Hello World</h1>
-      <Button className="hover:bg-blue-300">Click me</Button>
+      <Button className="">Click me!</Button>
     </div>
   );
 }
